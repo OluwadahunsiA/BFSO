711568cf06bf501d893b5c9d634bf91dfb7af044
Oluwadahunsi <oluwadahunsi@Oluwadahunsis-MacBook-Air.local>
2023-09-28 12:49:05 +0300

    update back

--- index.js
+++ index.js
@@ -19,10 +19,26 @@ noteSchema.set('toJSON', {
   },
 });
 
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'matformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
 app.use(cors());
-app.use(express.json());
 
 app.use(express.static('dist'));
+app.use(express.json());
 
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
@@ -53,31 +69,49 @@ app.post('/api/notes', (request, response) => {
   });
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      return next(error);
+    });
 });
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  response.status(204).end();
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.put('/api/notes/:id', (request, response) => {
-  Note.findOneAndUpdate({ _id: request.params.id }, [
-    { $set: { important: { $not: '$important' } } },
-  ]).then((resp) => {
-    response.json(resp);
-  });
-  // Note.updateOne({ id: request.params.id });
-  // const id = Number(request.params.id);
-  // const changeImportance = notes.find((note) => note.id === id);
-  // changeImportance.important = !changeImportance.important;
-  // notes = notes.map((note) => (note.id === id ? changeImportance : note));
-  // response.send(changeImportance);
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
+
+app.use(errorHandler);
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT;
 
